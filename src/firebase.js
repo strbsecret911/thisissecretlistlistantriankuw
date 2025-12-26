1e5a6fc7171660094affe48f67106c7932a680e5
strbsecret911 <kiosinternett@gmail.com>
2025-12-27 02:11:47 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Firebase config (punya kamu, AMAN dipakai)
 const firebaseConfig = {
   apiKey: "AIzaSyBN2PxHOA9u-I2iPCX_5gT1iogL5zYGHhM",
   authDomain: "antrianky.firebaseapp.com",
   projectId: "antrianky",
-  storageBucket: "antrianky.firebasestorage.app",
+  storageBucket: "antrianky.appspot.com",
   messagingSenderId: "194560917480",
-  appId: "1:194560917480:web:1aa1faa95f7e30c747ae5d",
+  appId: "1:194560917480:web:99c4bc99bfb9fcc747ae5d"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Services
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
