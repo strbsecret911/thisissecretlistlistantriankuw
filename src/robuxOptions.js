53c818d161ecf920d767015fb86ae59bee1205d3
strbsecret911 <kiosinternett@gmail.com>
2025-12-27 02:13:24 +0700

    Create robuxOptions.js

---
+++ src/robuxOptions.js
@@ -0,0 +1,17 @@
+export const ROBUX = {
+  REGULER: [
+    "80 Robux","160 Robux","240 Robux","320 Robux","400 Robux","480 Robux",
+    "560 Robux","640 Robux","720 Robux","800 Robux","1.700 Robux","2.100 Robux",
+    "3.400 Robux","4.500 Robux","10.000 Robux","22.500 Robux"
+  ],
+  BASIC: [
+    "500 Robux","580 Robux","660 Robux","740 Robux","820 Robux","1.000 Robux",
+    "1.500 Robux","2.000 Robux","2.500 Robux","3.000 Robux","3.500 Robux",
+    "4.000 Robux","5.000 Robux","6.000 Robux","15.000 Robux"
+  ],
+  PREMIUM: [
+    "450 Robux + Premium","1.000 Robux + Premium","1.550 Robux + Premium",
+    "2.200 Robux + Premium","2.750 Robux + Premium","3.300 Robux + Premium",
+    "4.400 Robux + Premium","5.500 Robux + Premium","11.000 Robux + Premium"
+  ]
+};
