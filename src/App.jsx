1f930438d97d2c72054ff7ff678d96d0261f650e
strbsecret911 <kiosinternett@gmail.com>
2026-03-15 14:32:15 +0700

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import PublicList from "./pages/PublicList.jsx";
-import Admin from "./pages/Admin.jsx";
-
-export default function App() {
-  const hash = window.location.hash || "#/";
-  const isAdmin = hash.startsWith("#/admin");
-  return isAdmin ? <Admin /> : <PublicList />;
-}
